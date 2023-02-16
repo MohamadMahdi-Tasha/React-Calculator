a737330f6753f8c982d75081f066d6dafff19124
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-16 12:55:21 +0330

    yea

---
+++ src/app.js
@@ -0,0 +1,21 @@
+// Codes By Mahdi Tasha
+import React, { Component }  from "react";
+
+class App extends Component {
+    constructor(prop) {
+        super(prop);
+
+        // this.state = {
+        // }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
