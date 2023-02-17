fa82841e3a43c4548be33f0c51ffaf1a2da77533
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-17 13:58:05 +0330

    yea

--- src/components/bottom-number-buttons.js
+++ src/components/bottom-number-buttons.js
@@ -5,7 +5,7 @@ function BottomNumberButtons(props) {
 
     function handleClick() {
         const calculatorOperationTxt = document.querySelector('.calculator__operation');
-        calculatorOperationTxt.textContent = calculatorOperationTxt.textContent + props.value;
+        calculatorOperationTxt.append(props.value);
     }
 
     return (<button onClick={handleClick} data-value={props.value} className={'calculator__number-btn'}>{props.value}</button>)
