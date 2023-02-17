9d61166df390884a69372694c33e89d6386c3949
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-17 12:07:44 +0330

    yea

---
+++ src/components/bottom-number-buttons.js
@@ -0,0 +1,10 @@
+// Codes By Mahdi Tasha
+import React from "react";
+
+function BottomNumberButtons(props) {
+    return (
+        <button>{props.value}</button>
+    )
+}
+
+export default BottomNumberButtons;
