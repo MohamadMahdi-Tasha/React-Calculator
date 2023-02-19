2737794214dde71559dbb0fe832489e555351051
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-19 19:37:09 +0330

    handled click of deg btn

--- src/components/bottom-number-buttons.js
+++ src/components/bottom-number-buttons.js
@@ -4,6 +4,12 @@ import React from "react";
 function BottomNumberButtons(props) {
     function handleClick() {
         const operationTxt = document.querySelector('.calculator__operation');
+        const equalTxt = document.querySelector('.calculator__equal');
+
+        if (equalTxt.textContent !== '') {
+            equalTxt.textContent = '';
+            operationTxt.textContent = ''
+        }
 
         operationTxt.textContent += props.value;
     }
