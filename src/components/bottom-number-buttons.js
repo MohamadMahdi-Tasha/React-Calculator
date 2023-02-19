4fdc1ab091a6a437e4586557b991163bc4fe5621
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-19 18:04:18 +0330

    yea

--- src/components/bottom-number-buttons.js
+++ src/components/bottom-number-buttons.js
@@ -2,13 +2,7 @@
 import React from "react";
 
 function BottomNumberButtons(props) {
-
-    function handleClick() {
-        const calculatorOperationTxt = document.querySelector('.calculator__operation');
-        calculatorOperationTxt.append(props.value);
-    }
-
-    return (<button onClick={handleClick} data-value={props.value} className={'calculator__number-btn'}>{props.value}</button>)
+    return <button data-value={props.value} className={'calculator__number-btn'}>{props.value}</button>
 }
 
 export default BottomNumberButtons;
